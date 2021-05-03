28dadd633a8646e26462cb050d146e29658bc0c6
KapishevSV <120671@mail.ru>
2021-05-04 00:00:53 +0500

    Initial commit

---
+++ src/main/java/edu/javacourse/student/dao/FacultyRepository.java
@@ -0,0 +1,15 @@
+package edu.javacourse.student.dao;
+
+import edu.javacourse.student.domain.Faculty;
+import edu.javacourse.student.domain.University;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface FacultyRepository extends JpaRepository<Faculty, Long>
+{
+    @Query(name = "Faculty.findFaculties",
+            value = "SELECT f FROM Faculty f LEFT JOIN FETCH f.university")
+    List<Faculty> findAll();
+}
